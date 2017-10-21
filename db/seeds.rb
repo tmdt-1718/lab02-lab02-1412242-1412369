e8934d19eed08a2b146349f01bfd65b023680d89
Lý Thành Nhân <mrfrenkyztiny@gmail.com>
2017-10-22 02:09:34 +0700

    Update Models

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,26 +7,37 @@ user1= User.create!(name:"ly thanh nhan",email:"nhan@gmail.com",password:"123123
 user2= User.create!(name:"nguyen quoc khanh",email:"khanh@gmail.com",password:"123123",avatar:"/avatar/khanh.jpg")
 user3= User.create!(name:"thai nguyen hoang phat",email:"phat@gmail.com",password:"123123",avatar:"/avatar/phat.jpg")
 
-cons1 = Conservation.create!(user1:user1.id,user2:user2.id)
-cons2 = Conservation.create!(user1:user1.id,user2:user3.id)
-cons3 = Conservation.create!(user1:user2.id,user2:user3.id)
+nhan_khanh = Conservation.create!(user1:user1.id,user2:user2.id,name:"nhan vs khanh")
+nhan_phat = Conservation.create!(user1:user1.id,user2:user3.id,name:"nhan vs phat")
+khanh_phat = Conservation.create!(user1:user2.id,user2:user3.id,name:"khanh vs phat")
 
-UserConservation.create!(user_id:user1.id,conservation_id:cons1.id)
-UserConservation.create!(user_id:user2.id,conservation_id:cons1.id)
+UserConservation.create!(user_id:user1.id,conservation_id:nhan_khanh.id)
+UserConservation.create!(user_id:user2.id,conservation_id:nhan_khanh.id)
 
-UserConservation.create!(user_id:user1.id,conservation_id:cons2.id)
-UserConservation.create!(user_id:user3.id,conservation_id:cons2.id)
-
-
-UserConservation.create!(user_id:user2.id,conservation_id:cons3.id)
-UserConservation.create!(user_id:user3.id,conservation_id:cons3.id)
+UserConservation.create!(user_id:user1.id,conservation_id:nhan_phat.id)
+UserConservation.create!(user_id:user3.id,conservation_id:nhan_phat.id)
 
 
-cons1.messages.create!(sender:user1.id,avatar:user1.avatar,content:"user1 sent to user2")
-cons1.messages.create!(sender:user2.id, avatar:user2.avatar, content:"user 2 sent to user1")
+UserConservation.create!(user_id:user2.id,conservation_id:khanh_phat.id)
+UserConservation.create!(user_id:user3.id,conservation_id:khanh_phat.id)
 
-cons2.messages.create!(sender:user1.id,avatar:user1.avatar,content:"user1 sent to user3")
-cons2.messages.create!(sender:user3.id, avatar:user3.avatar, content:"user 3 sent to user1")
 
-cons3.messages.create!(sender:user2.id,avatar:user2.avatar,content:"user2 sent to user3")
-cons3.messages.create!(sender:user3.id, avatar:user3.avatar, content:"user 3 sent to user2")
+nhan_khanh.messages.create!(sender:user1.id,avatar:user1.avatar,content:"#{user1.name} --> #{user2.name}")
+nhan_khanh.messages.create!(sender:user2.id, avatar:user2.avatar, content:"#{user2.name} --> #{user1.name}")
+nhan_khanh.messages.create!(sender:user1.id,avatar:user1.avatar,content:"#{user1.name} --> #{user2.name}")
+nhan_khanh.messages.create!(sender:user2.id, avatar:user2.avatar, content:"#{user2.name} --> #{user1.name}")
+nhan_khanh.messages.create!(sender:user1.id,avatar:user1.avatar,content:"#{user1.name} --> #{user2.name}")
+nhan_khanh.messages.create!(sender:user2.id, avatar:user2.avatar, content:"#{user2.name} --> #{user1.name}")
+nhan_khanh.messages.create!(sender:user1.id,avatar:user1.avatar,content:"#{user1.name} --> #{user2.name}")
+nhan_khanh.messages.create!(sender:user2.id, avatar:user2.avatar, content:"#{user2.name} --> #{user1.name}")
+nhan_khanh.messages.create!(sender:user1.id,avatar:user1.avatar,content:"#{user1.name} --> #{user2.name}")
+nhan_khanh.messages.create!(sender:user2.id, avatar:user2.avatar, content:"#{user2.name} --> #{user1.name}")
+
+
+
+
+nhan_phat.messages.create!(sender:user1.id,avatar:user1.avatar,content:"#{user1.name} --> #{user3.name}")
+nhan_phat.messages.create!(sender:user3.id, avatar:user3.avatar, content:"#{user3.name} --> #{user1.name}")
+
+khanh_phat.messages.create!(sender:user2.id,avatar:user2.avatar,content:"#{user2.name}--> #{user3.name}")
+khanh_phat.messages.create!(sender:user3.id, avatar:user3.avatar, content:"#{user3.name} --> #{user2.name}")
