ddf73c7c34ec1a6ea7e42e5c613fd9a1aea73142
INSPIRON15\baotr_000 <nqkhanh291996@gmail.com>
2017-10-23 22:45:46 +0700

    Update missing file for login-register

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,21 @@
+class UsersController < ApplicationController
+    skip_before_action :require_login, only: [:index, :create]
+    def index
+        @new_user = User.new 
+    end
+
+    def create
+        @user = User.new(user_params)
+        if @user.save
+        flash[:success] = "Register success"
+        redirect_to login_path
+        else
+        flash[:success] = "Register failed"
+        end
+    end
+
+    private
+    def user_params
+        params.require(:user).permit :name, :email, :password, :password_confirmation
+    end
+end
