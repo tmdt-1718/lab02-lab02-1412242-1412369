08240cf98434be997f6d37ed7265cf6f2f64db65
Lý Thành Nhân <mrfrenkyztiny@gmail.com>
2017-10-26 00:47:06 +0700

    Merge branch 'master' into origin/master

--- app/controllers/friends_controller.rb
+++ app/controllers/friends_controller.rb
@@ -3,18 +3,36 @@ class FriendsController < ApplicationController
         @new_friend = Friend.new 
         @del_friend = Friend.new 
         if current_user
+            @crrent_user = current_user
             list1 = Friend.where(user1: current_user.email)
             list2 = Friend.where(user2: current_user.email)
             @list = Array.new
+            @list_cons= Array.new
             list1.each {|x| 
                 user = User.find_by email: x.user2
                 if user
+                    @conservation.create(
+                        user1:current_user.id,
+                        user1_name:current_user.name,
+                        user2:user.id,
+                        user2_name:user.name,
+                        is_read:"false"
+                        )
+                    @list_cons.push(conservation)
                     @list.push(user)
                 end
             }
             list2.each {|x| 
                 user = User.find_by email: x.user1
                 if user
+                    @conservation.create(
+                        user1:current_user.id,
+                        user1_name:current_user.name,
+                        user2:user.id,
+                        user2_name:user.name,
+                        is_read:"false"
+                    )
+                    @list_cons.push(conservaion)
                     @list.push(user)
                 end
             }
@@ -28,9 +46,10 @@ class FriendsController < ApplicationController
         list2 = Friend.where(user2: current_user.email)
         is_exist1= list1.find {|e| e.user2 == user.email} 
         is_exist2= list2.find {|e| e.user1 == user.email} 
+      
         if user && (current_user != user) && !is_exist1 && !is_exist2
             friend.user1 = current_user.email
-            friend.save()
+            friend.save()            
             redirect_to friends_path
         end
     end
--- db/seeds.rb
+++ db/seeds.rb
@@ -3,42 +3,42 @@ Message.delete_all
 Conservation.delete_all
 UserConservation.delete_all
 
-user1= User.create!(name:"ly thanh nhan",email:"nhan@gmail.com",password:"123123",avatar:"/avatar/nhan.jpg")
-user2= User.create!(name:"nguyen quoc khanh",email:"khanh@gmail.com",password:"123123",avatar:"/avatar/khanh.jpg")
-user3= User.create!(name:"thai nguyen hoang phat",email:"phat@gmail.com",password:"123123",avatar:"/avatar/phat.jpg")
+# user1= User.create!(name:"ly thanh nhan",email:"nhan@gmail.com",password:"123123",avatar:"/avatar/nhan.jpg")
+# user2= User.create!(name:"nguyen quoc khanh",email:"khanh@gmail.com",password:"123123",avatar:"/avatar/khanh.jpg")
+# user3= User.create!(name:"thai nguyen hoang phat",email:"phat@gmail.com",password:"123123",avatar:"/avatar/phat.jpg")
 
-nhan_khanh = Conservation.create!(user1:user1.id,user2:user2.id,user1_name: user1.name,user2_name: user2.name,name:"nhan vs khanh",is_read:"false")
-nhan_phat = Conservation.create!(user1:user1.id,user2:user3.id,user1_name: user1.name,user2_name: user3.name,name:"nhan vs phat", is_read:"true")
-khanh_phat = Conservation.create!(user1:user2.id,user2:user3.id,user1_name: user2.name,user2_name: user3.name,name:"khanh vs phat", is_read:"false")
+# nhan_khanh = Conservation.create!(user1:user1.id,user2:user2.id,user1_name: user1.name,user2_name: user2.name,name:"nhan vs khanh",is_read:"false")
+# nhan_phat = Conservation.create!(user1:user1.id,user2:user3.id,user1_name: user1.name,user2_name: user3.name,name:"nhan vs phat", is_read:"true")
+# khanh_phat = Conservation.create!(user1:user2.id,user2:user3.id,user1_name: user2.name,user2_name: user3.name,name:"khanh vs phat", is_read:"false")
 
-UserConservation.create!(user_id:user1.id,conservation_id:nhan_khanh.id)
-UserConservation.create!(user_id:user2.id,conservation_id:nhan_khanh.id)
+# UserConservation.create!(user_id:user1.id,conservation_id:nhan_khanh.id)
+# UserConservation.create!(user_id:user2.id,conservation_id:nhan_khanh.id)
 
-UserConservation.create!(user_id:user1.id,conservation_id:nhan_phat.id)
-UserConservation.create!(user_id:user3.id,conservation_id:nhan_phat.id)
-
-
-UserConservation.create!(user_id:user2.id,conservation_id:khanh_phat.id)
-UserConservation.create!(user_id:user3.id,conservation_id:khanh_phat.id)
+# UserConservation.create!(user_id:user1.id,conservation_id:nhan_phat.id)
+# UserConservation.create!(user_id:user3.id,conservation_id:nhan_phat.id)
 
 
-nhan_khanh.messages.create!(is_read:"false",sender:user1.id,avatar:user1.avatar,sender_name: user1.name,content:"#{user1.name} --> #{user2.name}")
-nhan_khanh.messages.create!(is_read:"false",sender:user2.id, avatar:user2.avatar,sender_name: user2.name, content:"#{user2.name} --> #{user1.name}")
-nhan_khanh.messages.create!(is_read:"false",sender:user1.id,avatar:user1.avatar,sender_name: user1.name,content:"#{user1.name} --> #{user2.name}")
-nhan_khanh.messages.create!(is_read:"true",sender:user2.id, avatar:user2.avatar,sender_name: user2.name, content:"#{user2.name} --> #{user1.name}")
-nhan_khanh.messages.create!(is_read:"false",sender:user1.id,avatar:user1.avatar,sender_name: user1.name, content:"#{user1.name} --> #{user2.name}")
-nhan_khanh.messages.create!(is_read:"false",sender:user2.id, avatar:user2.avatar,sender_name: user2.name, content:"#{user2.name} --> #{user1.name}")
-nhan_khanh.messages.create!(is_read:"false",sender:user1.id,avatar:user1.avatar,sender_name: user1.name,content:"#{user1.name} --> #{user2.name}")
-nhan_khanh.messages.create!(is_read:"false",sender:user2.id, avatar:user2.avatar,sender_name: user2.name, content:"#{user2.name} --> #{user1.name}")
-nhan_khanh.messages.create!(is_read:"false",sender:user1.id,avatar:user1.avatar,sender_name: user1.name, content:"#{user1.name} --> #{user2.name}")
-nhan_khanh.messages.create!(is_read:"true",sender:user2.id, avatar:user2.avatar,sender_name: user2.name, content:"#{user2.name} --> #{user1.name}")
-nhan_khanh.messages.create!(is_read:"true",sender:user2.id, avatar:user2.avatar,sender_name: user2.name, content:"#{user2.name} --> #{user1.name}")
+# UserConservation.create!(user_id:user2.id,conservation_id:khanh_phat.id)
+# UserConservation.create!(user_id:user3.id,conservation_id:khanh_phat.id)
+
+
+# nhan_khanh.messages.create!(is_read:"false",sender:user1.id,avatar:user1.avatar,sender_name: user1.name,content:"#{user1.name} --> #{user2.name}")
+# nhan_khanh.messages.create!(is_read:"false",sender:user2.id, avatar:user2.avatar,sender_name: user2.name, content:"#{user2.name} --> #{user1.name}")
+# nhan_khanh.messages.create!(is_read:"false",sender:user1.id,avatar:user1.avatar,sender_name: user1.name,content:"#{user1.name} --> #{user2.name}")
+# nhan_khanh.messages.create!(is_read:"true",sender:user2.id, avatar:user2.avatar,sender_name: user2.name, content:"#{user2.name} --> #{user1.name}")
+# nhan_khanh.messages.create!(is_read:"false",sender:user1.id,avatar:user1.avatar,sender_name: user1.name, content:"#{user1.name} --> #{user2.name}")
+# nhan_khanh.messages.create!(is_read:"false",sender:user2.id, avatar:user2.avatar,sender_name: user2.name, content:"#{user2.name} --> #{user1.name}")
+# nhan_khanh.messages.create!(is_read:"false",sender:user1.id,avatar:user1.avatar,sender_name: user1.name,content:"#{user1.name} --> #{user2.name}")
+# nhan_khanh.messages.create!(is_read:"false",sender:user2.id, avatar:user2.avatar,sender_name: user2.name, content:"#{user2.name} --> #{user1.name}")
+# nhan_khanh.messages.create!(is_read:"false",sender:user1.id,avatar:user1.avatar,sender_name: user1.name, content:"#{user1.name} --> #{user2.name}")
+# nhan_khanh.messages.create!(is_read:"true",sender:user2.id, avatar:user2.avatar,sender_name: user2.name, content:"#{user2.name} --> #{user1.name}")
+# nhan_khanh.messages.create!(is_read:"true",sender:user2.id, avatar:user2.avatar,sender_name: user2.name, content:"#{user2.name} --> #{user1.name}")
 
 
 
 
-nhan_phat.messages.create!(is_read:"true",sender:user1.id,avatar:user1.avatar,sender_name:user1.name,content:"#{user1.name} --> #{user3.name}")
-nhan_phat.messages.create!(is_read:"true",sender:user3.id, avatar:user3.avatar,sender_name:user3.name, content:"#{user3.name} --> #{user1.name}")
+# nhan_phat.messages.create!(is_read:"true",sender:user1.id,avatar:user1.avatar,sender_name:user1.name,content:"#{user1.name} --> #{user3.name}")
+# nhan_phat.messages.create!(is_read:"true",sender:user3.id, avatar:user3.avatar,sender_name:user3.name, content:"#{user3.name} --> #{user1.name}")
 
-khanh_phat.messages.create!(is_read:"true",sender:user2.id,avatar:user2.avatar,sender_name:user2.name,content:"#{user2.name}--> #{user3.name}")
-khanh_phat.messages.create!(is_read:"true",sender:user3.id, avatar:user3.avatar,sender_name:user3.name,content:"#{user3.name} --> #{user2.name}")
+# khanh_phat.messages.create!(is_read:"true",sender:user2.id,avatar:user2.avatar,sender_name:user2.name,content:"#{user2.name}--> #{user3.name}")
+# khanh_phat.messages.create!(is_read:"true",sender:user3.id, avatar:user3.avatar,sender_name:user3.name,content:"#{user3.name} --> #{user2.name}")
